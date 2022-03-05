0dcb30bcb45a782f589bf91d8a93c4d079a3e631
bj <jariwala24b@gmail.com>
2022-03-05 21:08:16 +0530

    navbar stage 1

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-menu');
+const navbar = document.querySelector('.navbar');
 //hamburger styling
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
+    navbar.classList.toggle('light-up');
 });
 document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
+    navbar.classList.toggle('light-up');
 }));
