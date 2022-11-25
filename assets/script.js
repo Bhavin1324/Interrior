05e64e7b599ac97e37c43248e6dde1877f8a6fd6
bhavin <bjariwala26@gmail.com>
2022-11-25 21:49:24 +0530

    loader added

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,74 @@
 AOS.init({
-    startEvent: 'DOMContentLoaded',
-    offset: 100,
-    duration: 1000,
+  startEvent: "DOMContentLoaded",
+  offset: 100,
+  duration: 1000,
+});
+
+window.addEventListener("load", () => {
+  document.querySelector(".loader").classList.remove("loader");
 });
 
 let elem = document.querySelector(".navbar");
 function scrollAction() {
-    console.log(window.scrollY);
-    elem.classList.toggle("scroll-active", window.scrollY > 0);
-    if (window.scrollY > 0) {
-        elem.classList.add("scroll-active");
-    }
-    else {
-        elem.classList.remove("scroll-active");
-    }
+  console.log(window.scrollY);
+  elem.classList.toggle("scroll-active", window.scrollY > 0);
+  if (window.scrollY > 0) {
+    elem.classList.add("scroll-active");
+  } else {
+    elem.classList.remove("scroll-active");
+  }
 }
 
-let hamburger = document.querySelector('.hamburger');
-let menu = document.querySelector('.nav-menu');
-let navbar = document.querySelector('.navbar');
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".nav-menu");
+let navbar = document.querySelector(".navbar");
 //hamburger styling
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-    navbar.classList.toggle('light-up');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+  navbar.classList.toggle("light-up");
 });
-document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-    navbar.classList.toggle('light-up');
-}));
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    navbar.classList.toggle("light-up");
+  })
+);
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth >= 1024) {
-        if (navbar.classList.contains('light-up')) {
-            navbar.classList.remove('light-up');
-        }
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1024) {
+    if (navbar.classList.contains("light-up")) {
+      navbar.classList.remove("light-up");
     }
-    else {
-        if (hamburger.classList.contains('active')) {
-            navbar.classList.add('light-up');
-        }
-        else {
-            navbar.classList.remove('light-up');
-        }
+  } else {
+    if (hamburger.classList.contains("active")) {
+      navbar.classList.add("light-up");
+    } else {
+      navbar.classList.remove("light-up");
     }
-})
+  }
+});
 
 //carousel
-let slides = document.querySelectorAll('.slide');
+let slides = document.querySelectorAll(".slide");
 let count = 0;
 function reset() {
-    slides.forEach(slide => slide.style.opacity = "0")
+  slides.forEach((slide) => (slide.style.opacity = "0"));
 }
 function slideShow() {
-    reset();
-    slides[0].style.opacity = "1";
-    setInterval(() => {
-        if (count === slides.length - 1) { count = -1 }
-        nextSlide();
-    }, 8000);
+  reset();
+  slides[0].style.opacity = "1";
+  setInterval(() => {
+    if (count === slides.length - 1) {
+      count = -1;
+    }
+    nextSlide();
+  }, 8000);
 }
 function nextSlide() {
-    reset();
-    slides[count + 1].style.opacity = "1";
-    count++;
+  reset();
+  slides[count + 1].style.opacity = "1";
+  count++;
 }
 slideShow();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: ["./assets/**/*.{html,js}","*"],
+  content: ["./assets/**/*.{html,js}", "*"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spin: {
+          "0%": {
+            transform: "translate(-50%, -50%) rotate(0deg)",
+          },
+          "100%": {
+            transform: "translate(-50%, -50%) rotate(360deg)",
+          },
+        },
+      },
+      animation: {
+        spin: "spin 1s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
